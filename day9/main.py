e7ca2f007be155c4cbe552bdf642577a3a3faca9
Leo Lee <leoohylee@gmail.com>
2024-12-09 16:27:17 +0000

    update day 9 (not finished)

--- day9/main.py
+++ day9/main.py
@@ -48,6 +48,65 @@ def calculate_checksum(disk):
         left += 1
     return checksum
 
-
 harddisk = read_input('./input.txt')
 print(calculate_checksum(expand_disk(harddisk)))
+
+# Part 2
+
+def expand_disk_v2(comapact_disk):
+    disk = []
+    space = {}
+    idx = 0
+    for pos, node in enumerate(comapact_disk):
+        if pos % 2 == 0:
+            for times in range(int(node)):
+                disk.append(idx)
+            idx += 1
+        else:
+            for times in range(int(node)):
+                disk.append('.')
+    return disk
+
+def reallocate_files_v2(disk):
+    right = len(disk) - 1
+    while right >= 0:
+        if disk[right] != '.':
+            file = disk[right]
+            file_size = 0
+            while disk[right] == file:
+                file_size += 1
+                disk[right] = '.'
+                right -= 1
+            left = 0
+            space = ''
+            while left < right and file_size > 0:
+                if disk[left] == '.':
+                    space = space + '.'
+                    if len(space) == file_size:
+                        for i in range(file_size):
+                            disk[left-i] = file
+                        file_size = 0
+                else:
+                    space = ''
+                left += 1
+            if file_size >0:
+                right += 1
+                for j in range(file_size):
+                    disk[right+j] = file
+                right -= 1
+        else:
+            right -= 1
+    return disk
+
+def calculate_checksum_v2(disk):
+    disk = reallocate_files_v2(disk)
+    checksum = 0
+    print(disk)
+    for i, file in enumerate(disk):
+        if file != '.':
+            checksum += file * i
+    return checksum
+
+
+harddisk = read_input('./input.txt')
+print(calculate_checksum_v2(expand_disk_v2(harddisk)))
